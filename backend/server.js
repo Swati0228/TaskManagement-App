3f2ccd8bd1d4a756891d2afa4fc9525cc06d9bd3
Swati Singh <rashmisingh2114782@gmail.com>
2025-07-31 23:49:10 +0530

    changing origin

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const secret = process.env.COOKIE_SECRET;
-const frontendBaseURL = process.env.FRONTEND_BASE_URL;
+// const frontendBaseURL = process.env.FRONTEND_BASE_URL;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(secret));
 app.use(
   cors({
-    origin: frontendBaseURL,
+    origin: "*",
     credentials: true,
   })
 );
